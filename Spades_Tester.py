b133b59c4ae9e0fd1fd1edc3f63b6412a7d3486c
riv-eting <vtrivedi1@luc.edu>
2024-02-28 02:16:55 -0600

    Update

--- Spades_Tester.py
+++ Spades_Tester.py
@@ -2,16 +2,16 @@ import Bio
 import os
 import glob
 from Bio import Entrez
+from Bio import SeqIO
+log=open('PipelineProject.log', 'w')
 inpath = '/home/vtrivedi1/Python-Pipeline-Vir-Trivedi/SRX*.fastq'
 fastqs = glob.glob(inpath)
 #x is a list of all fastq paths from the directory being used
-print(fastqs)
 fastq_labels=[]
 for path in fastqs:
     fastq_labels.append(path[-18:-8])
 fastq_labels=set(fastq_labels)
 fastq_donor_dict = {}
-print(fastq_labels)
 for label in fastq_labels:
     if label == 'SRX2896375':
         fastq_donor_dict[label]='Donor 3 (6dpi)'
@@ -21,5 +21,45 @@ for label in fastq_labels:
         fastq_donor_dict[label]='Donor 1 (2dpi)'
     elif label == 'SRX2896374':
         fastq_donor_dict[label]='Donor 3 (2dpi)'
+#Work through writing to log file logic?
 mapped = glob.glob('/home/vtrivedi1/Python-Pipeline-Vir-Trivedi/SRX*_mapped*.fq.gz')
-cool = 'spades.py -k 77,99,127 -t 2 --only-assembler -1 SRR5364281_1.fastq -2 SRR5364281_2.fastq -o SRR5364281_assembly/'
+total_reads=list(mapped+fastqs)
+for label in fastq_labels:
+    precount_1=0
+    precount_2=0
+    count_1=0
+    count_2=0
+    for file in total_reads:
+        if file == f'/home/vtrivedi1/Python-Pipeline-Vir-Trivedi/{label}_1.fastq':
+            '''
+            with open(str(file), 'r') as handle:
+                for record in SeqIO.parse(handle, 'fastq'):
+                    precount_1 += 1
+            '''
+        if file == f'/home/vtrivedi1/Python-Pipeline-Vir-Trivedi/{label}_2.fastq':
+            '''
+            with open(str(file), 'r') as handle:
+                for record in SeqIO.parse(handle, 'fastq'):
+                    precount_2 += 1
+            '''
+        if file == f'/home/vtrivedi1/Python-Pipeline-Vir-Trivedi/{label}_mapped_1.fq.gz':
+            '''
+            with open(str(file), 'r') as handle:
+                for record in SeqIO.parse(handle, 'fastq'):
+                    count_1 += 1
+            '''
+        if file == f'/home/vtrivedi1/Python-Pipeline-Vir-Trivedi/{label}_mapped_2.fq.gz':
+            '''
+            with open(str(file), 'r') as handle:
+                for record in SeqIO.parse(handle, 'fastq'):
+                    count_2 += 1
+            '''
+    #print(f'{fastq_donor_dict[label]} had {precount_1 + precount_2} reads before Bowtie2 filtering and {count_1 + count_2} read pairs after.')
+    #ASK WHEELER
+'''
+with open(fastq_file, "r") as handle:
+        for record in SeqIO.parse(handle, "fastq"):
+            count += 1
+    return count
+'''
+os.system('spades.py --only-assembler --pe1-1 -o pipeline_outputs/')
